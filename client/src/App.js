739bb0f8807438dd021f80f6932e82b02be20c9e
Cavinkumaran M <82271718+Cavinkumaran-m@users.noreply.github.com>
2023-04-30 21:17:11 +0530

    basic bg update

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
-import "./App.css";
+import style from "./App.module.css";
 import RightWindow from "./components/cavin/RightWindow";
 import LeftMenu from "./components/deepak/LeftMenu";
 
 function App() {
   return (
-    <>
+    <div className={style.Container}>
       <LeftMenu></LeftMenu>
       <RightWindow></RightWindow>
-    </>
+    </div>
   );
 }
 
--- client/src/components/cavin/RightWindow.js
+++ client/src/components/cavin/RightWindow.js
@@ -1,5 +1,11 @@
+import style from "./RightWindow.module.css";
+
 function RightWindow() {
-  return;
+  return (
+    <div className={style.mainDiv}>
+      <h1>Right Menu to be designed by Cavin</h1>
+    </div>
+  );
 }
 
 export default RightWindow;
