54df07c81a18f384617dd52d4471c3bcd5fca971
Cavinkumaran M <82271718+Cavinkumaran-m@users.noreply.github.com>
2023-05-01 11:28:10 +0530

    a

--- client/src/components/cavin/Header/Header.js
+++ client/src/components/cavin/Header/Header.js
@@ -5,13 +5,23 @@ function Header() {
     <div className={style.header}>
       {/* <div className={style.imgContainer}> */}
       <img src="https://pbs.twimg.com/media/DlnpurZW0AI5uk6.jpg:small"></img>
-      <span>Rae Lil Black</span>
-      <svg viewBox="0 0 24 24" height="24" width="24">
-        <path
-          fill="currentColor"
-          d="M15.9,14.3H15L14.7,14c1-1.1,1.6-2.7,1.6-4.3c0-3.7-3-6.7-6.7-6.7S3,6,3,9.7 s3,6.7,6.7,6.7c1.6,0,3.2-0.6,4.3-1.6l0.3,0.3v0.8l5.1,5.1l1.5-1.5L15.9,14.3z M9.7,14.3c-2.6,0-4.6-2.1-4.6-4.6s2.1-4.6,4.6-4.6 s4.6,2.1,4.6,4.6S12.3,14.3,9.7,14.3z"
-        ></path>
-      </svg>
+      <span className={style.name}>Rae Lil Black</span>
+      <span className={style.glass}>
+        <svg viewBox="0 0 24 24" height="24" width="24" x="0px" y="0px">
+          <path
+            fill="white"
+            d="M15.9,14.3H15L14.7,14c1-1.1,1.6-2.7,1.6-4.3c0-3.7-3-6.7-6.7-6.7S3,6,3,9.7 s3,6.7,6.7,6.7c1.6,0,3.2-0.6,4.3-1.6l0.3,0.3v0.8l5.1,5.1l1.5-1.5L15.9,14.3z M9.7,14.3c-2.6,0-4.6-2.1-4.6-4.6s2.1-4.6,4.6-4.6 s4.6,2.1,4.6,4.6S12.3,14.3,9.7,14.3z"
+          ></path>
+        </svg>
+      </span>
+      <span className={style.options}>
+        <svg viewBox="0 0 24 24" height="24" width="24" x="0px" y="0px">
+          <path
+            fill="white"
+            d="M12,7c1.104,0,2-0.896,2-2c0-1.105-0.895-2-2-2c-1.104,0-2,0.894-2,2 C10,6.105,10.895,7,12,7z M12,9c-1.104,0-2,0.894-2,2c0,1.104,0.895,2,2,2c1.104,0,2-0.896,2-2C13.999,9.895,13.104,9,12,9z M12,15 c-1.104,0-2,0.894-2,2c0,1.104,0.895,2,2,2c1.104,0,2-0.896,2-2C13.999,15.894,13.104,15,12,15z"
+          ></path>
+        </svg>
+      </span>
     </div>
     // </div>
   );
