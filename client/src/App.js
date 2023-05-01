e2b50193ebc3a26397ef957aed7201beb4721599
Dhiyanesh2003 <dhiyanesh7338942092@gmail.com>
2023-05-01 10:26:51 +0530

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,10 @@
-<<<<<<< Updated upstream
 import { BrowserRouter as Router } from "react-router-dom";
 import AllRoutes from "./AllRoutes";
 import './App.css';
-=======
 import style from "./App.module.css";
 import RightWindow from "./components/cavin/RightWindow";
 import LeftMenu from "./components/deepak/LeftMenu";
 import { useEffect } from "react";
->>>>>>> Stashed changes
 
 function App() {
   return (
