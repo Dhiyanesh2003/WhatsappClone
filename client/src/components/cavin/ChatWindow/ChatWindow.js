91634aac245ed8f57f80d8c306e86ae951cd2d2c
Cavinkumaran M <82271718+Cavinkumaran-m@users.noreply.github.com>
2023-05-01 15:09:21 +0530

    completed...

--- client/src/components/cavin/ChatWindow/ChatWindow.js
+++ client/src/components/cavin/ChatWindow/ChatWindow.js
@@ -3,27 +3,13 @@ import MsgSend from "./MsgSend.js";
 import MsgReceive from "./MsgReceive";
 
 function ChatWindow(props) {
-  // const checker = (convo) => {
-  //   if (convo["type"] == "send") {
-  //     return <MsgSend data={convo}></MsgSend>;
-  //   } else {
-  //     return <MsgReceive data={convo}></MsgReceive>;
-  //   }
-  // };
-  // console.log(
-  //   props.data.map((item) => {
-  //     return item["type"] == "send";
-  //   })
-  // );
-  // const Content = props.conversations.filter(checker);
-
   return (
     <div className={style.mainDiv}>
       {props.data.map((item) => {
         if (item["type"] == "send") {
-          return <MsgSend data={item}></MsgSend>;
+          return <MsgSend key={item.id} data={item}></MsgSend>;
         } else {
-          return <MsgReceive data={item}></MsgReceive>;
+          return <MsgReceive key={item.id} data={item}></MsgReceive>;
         }
       })}
     </div>
